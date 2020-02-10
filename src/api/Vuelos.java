ebe7ce9da792ecd55a4dd627a5da52b404c65c78
Carlos Rey Gonzalez <carlos.rey@live.u-tad.com>
2020-02-10 14:43:33 +0100

    java

---
+++ src/api/Vuelos.java
@@ -0,0 +1,10 @@
+package api;
+
+public class Vuelos {
+	
+
+
+	
+	
+
+}
---
+++ src/vuelos/ApiVuelos.java
@@ -0,0 +1,11 @@
+package vuelos;
+
+public interface ApiVuelos {
+	
+	String origenDestino();
+	String cualquierDestino();
+	String comprarPlaza();
+
+	
+
+}
